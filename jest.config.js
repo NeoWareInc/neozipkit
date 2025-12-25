6959835be2b775dfb610f1a3138e61dede12582e
Steven Burg <steve@nutecdev.com>
2025-12-25 10:18:10 -0500

    Cleanup Unit tests

--- jest.config.js
+++ jest.config.js
@@ -4,24 +4,11 @@ module.exports = {
   roots: ['<rootDir>/tests'],
   testMatch: ['**/*.test.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  collectCoverageFrom: [
-    'src/**/*.ts',
-    '!src/**/*.d.ts',
-    '!src/**/index.ts'
-  ],
   transform: {
     '^.+\\.ts$': ['ts-jest', {
       tsconfig: 'tsconfig.test.json'
     }]
   },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
-  },
-  // Increase timeout for tests that may take longer (like crypto operations)
-  testTimeout: 30000,
-  // Ignore node_modules except for ESM modules that need transformation
-  transformIgnorePatterns: [
-    'node_modules/(?!(@oneidentity/zstd-js)/)'
-  ]
+  testTimeout: 30000
 };
 
